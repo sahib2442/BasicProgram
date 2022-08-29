7fdb23bdc48ed022f3b660be8c8676506206a270
sahib2442 <kadersahib1234@gmail.com>
2022-08-29 15:24:55 +0530

    [sahib] Add: prime number

---
+++ PrimeNumber/PrimeNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace PrimeNumber
+{
+    public class PrimeNumber
+    {
+        public void PrimeCheck()
+        {
+            Console.WriteLine("Enter the number to check either its prime num or not");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                if (num%i == 0)
+                {
+                    Count++;
+                }
+            }
+            if (Count == 2)
+            {
+                Console.WriteLine("its a prime number");
+            }
+        }
+    }
+}
---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace PrimeNumber
+{
+    class program
+    {
+        static void main(string[]args)
+        {
+            PrimeNumber primeNUm = new PrimeNumber();
+            primeNUm.PrimeCheck();
+        }
+    }
+}
