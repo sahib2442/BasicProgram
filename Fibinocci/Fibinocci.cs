eb4ec8ac16a4bb4c76a3c5696313af730179faf5
sahib2442 <kadersahib1234@gmail.com>
2022-08-29 14:45:34 +0530

    [sahib] Add: fibinocci series

---
+++ Fibinocci/Fibinocci.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace fibinocciseries
+{
+    public class fibinocciseries
+    {
+        int firstNum = 0, secondNum = 0, thirdNum = 0;
+        public void print()
+        {
+            Console.WriteLine("Enter the number to print fibinocciseries");
+            int num = Convert.ToInt32(Console.ReadLine());
+            while (num > 0)
+            {
+                thirdNum = firstNum + secondNum;
+                firstNum = secondNum;
+                secondNum = thirdNum;
+                num--;
+            }
+            Console.WriteLine((firstNum +"\n"+ secondNum +"\n"+ thirdNum));
+        }
+    }
+}
---
+++ Fibinocci/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace fibinocciseries
+{
+    class program
+    {
+        static void main(string[]args)
+        {
+            fibinocciseries fibinocci = new fibinocciseries();
+            fibinocci.print();
+        }
+    }
+}
