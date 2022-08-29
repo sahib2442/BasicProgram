535c7745fe083000b972a2fec54304d20b51d9b4
sahib2442 <kadersahib1234@gmail.com>
2022-08-29 15:10:28 +0530

    [sahib] Add: perfect number

---
+++ PerfectNumber/PerfectNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace perfectNumber
+{
+    public class perfectNumber
+    {
+        int numCheck = 0;
+        public void perfectNumCheck()
+        {
+            Console.WriteLine("Enter the number to check either its perfectNum or not");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <=num/2; i++)
+            {
+                if (num%i == 0)
+                {
+                    numCheck += i;
+                }
+            }
+            if (numCheck == num)
+            {
+                Console.WriteLine("perfectNum");
+            }
+        }
+
+    }
+}
---
+++ PerfectNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace perfectNumber
+{
+    class program
+    {
+        static void main(string[]args)
+        {
+            perfectNumber perfectNum = new perfectNumber();
+            perfectNum.perfectNumCheck();
+        }
+    }
+}
