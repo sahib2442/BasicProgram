0576dd417c8c8fa98ffc5c6315661a385780ab96
sahib2442 <kadersahib1234@gmail.com>
2022-08-29 15:37:20 +0530

    [sahib] Add: reverse number
    clear

---
+++ ReverseNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ReverseNumber
+{
+    class program
+    {
+        static void main(string[]args)
+        {
+            ReverseNumber Reversenum = new ReverseNumber;
+            Reversenum.NumberCheck();
+        }
+    }
+}
---
+++ ReverseNumber/ReverseNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ReverseNumber
+{
+    public class ReverseNumber
+    {
+        int reverseNum=0;
+        public void NumberCheck()
+        {
+            Console.WriteLine("Enter Number");
+            int num =Convert.ToInt32(Console.ReadLine());
+            while(num > 0)
+            {
+                int reminder = num % 10;
+                reverseNum = reverseNum * 10 + reminder;
+                num = num / 10;
+            }
+            Console.WriteLine(reverseNum);
+        }
+    }
+}
